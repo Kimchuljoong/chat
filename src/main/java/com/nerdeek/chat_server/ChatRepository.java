0c1354566ee80ef9cac96a752d53f6f1b5a53647
kimchj <wnmh123@gmail.com>
2022-02-18 23:09:14 +0900

    chat project first commit chat repository, controller, model

---
+++ src/main/java/com/nerdeek/chat_server/ChatRepository.java
@@ -0,0 +1,15 @@
+package com.nerdeek.chat_server;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Tailable;
+
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends MongoRepository<Chat, String> {
+
+    @Tailable
+    @Query("{sender:?0, receiver:?1}")
+    Flux<Chat> mFindBySender(String sender, String receiver); // Flux (흐름) response 유지하면서 데이터 계속 보낼 수 있음
+
+}
